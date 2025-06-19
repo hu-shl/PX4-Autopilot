0d0455e8f521fb0c1e506da6b8be7a65d7a636b7
JannickBlmndl <jannick.bloemendal@gmail.com>
2025-06-19 12:52:47 +0200

    Add modules ID enums to rs_can common

--- src/modules/robosub/rs_canfd_common/rs_canfd_common.hpp
+++ src/modules/robosub/rs_canfd_common/rs_canfd_common.hpp
@@ -4,3 +4,15 @@ union converter{
     uint8_t bytes[4];
     float value;
 };
+
+// Enum of available HU Robosub module IDs
+typedef enum
+{
+  GLOBAL    = 0x00,
+  PIXHAWK   = 0x10,
+  MAINBRAIN = 0x11,
+  BUOYANCY  = 0x04,
+  HYDRAULIC = 0X05,
+  POWER     = 0x08,
+  TEST      = 0x0F
+} modules;
